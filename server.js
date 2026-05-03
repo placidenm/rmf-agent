1774ddd4ddc6121d7db058db8a13fcdc94753bba
sena <placidenm@gmail.com>
2026-05-03 17:50:21 -0400

    Fix public index path for render deplyment

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.static("public"));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
